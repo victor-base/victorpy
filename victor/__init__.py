a7b5d8cff50df9a97b6cd71e9495d09e5bb406b4
Emiliano A. Billi <emiliano.billi@gmail.com>
2025-04-27 08:42:52 -0300

    Update __init__.py

--- victor/__init__.py
+++ victor/__init__.py
@@ -1,3 +1,13 @@
 from .victor import VictorIndex
 
-__version__ = "0.1.0"
+
+# Distance methods
+L2NORM = 0x00
+COSINE = 0x01
+DOTP   = 0x02
+
+# Index types
+FLAT_INDEX = 0x00
+NSW_INDEX  = 0x02
+
+# Package version
